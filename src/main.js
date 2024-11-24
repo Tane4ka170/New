6cdf93f327fdac64a71cabc264cc86189e3646be
Tane4ka170 <tane4ka170@gmail.com>
2024-11-24 19:08:31 +0200

    Integrated Pinia and Router, added global styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "animate.css";
 
-createApp(App).mount('#app')
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
