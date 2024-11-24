39b39bb8a24b7b28dd653f3efb165a00897a0200
Tane4ka170 <tane4ka170@gmail.com>
2024-11-24 19:08:00 +0200

    Added search bar to top navigation

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
-<script setup></script>
+<script setup>
+import Magnify from "vue-material-design-icons/Magnify.vue";
+</script>
 
-<template>Hello</template>
+<template>
+  <div
+    id="TopNav"
+    class="fixed right-0 flex items-center justify-between w-[calc(100%-240px)] h-[56px] border-b border-b-[#1781f0]"
+  >
+    <div class="flex items-center w-full">
+      <Magnify class="pl-6 mt-1 pr-2" fillColor="#fffb08" />
+    </div>
+  </div>
+</template>
