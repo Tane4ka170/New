fec059384b981a8e3db37a37d5d94c24e4501db8
Tane4ka170 <tane4ka170@gmail.com>
2024-11-25 22:03:13 +0200

    CreatedHomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="max-w-[1500px] mx-auto">
+    <div class="px-8 mt-8 min-v-[800px] w-full">
+      <div class="text-white text-xl font-semibold inline-block">
+        Blends influenced by...
+        <div class="text-sm font-normal text-[#1781f0]">
+          Find new tracks that match your favorites
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
