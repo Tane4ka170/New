a03f6f62a537db768cbf73d7bd00033cf526d1bb
Tane4ka170 <tane4ka170@gmail.com>
2024-11-26 19:43:51 +0200

    Added BlendsInfluencedBy component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import BlendsInfluencedBy from "../components/BlendsInfluencedBy.vue";
+</script>
 
 <template>
   <div class="max-w-[1500px] mx-auto">
@@ -7,6 +9,20 @@
         Blends influenced by...
         <div class="text-sm font-normal text-[#1781f0]">
           Find new tracks that match your favorites
+        </div>
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="flex justify-between w-full gap-8">
+        <div class="xl:w-1/3 w-1/2">
+          <BlendsInfluencedBy
+            class="pb-1.5"
+            text="Only Teardrops"
+            by="Emilie de Forest"
+            to="/artist"
+            image="https://is1-ssl.mzstatic.com/image/thumb/Music2/v4/0c/c8/0f/0cc80f43-d7d1-31b5-724a-8b41b22787de/886443998551.jpg/1200x1200bb.jpg"
+          />
         </div>
       </div>
     </div>
