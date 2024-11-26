77ed5ba1133c5205431aa92db86a244f49312623
Tane4ka170 <tane4ka170@gmail.com>
2024-11-26 19:41:06 +0200

    Created BlendsInfluencedBy component

---
+++ src/components/BlendsInfluencedBy.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { toRefs, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+import Play from "vue-material-design-icons/Play.vue";
+
+const route = useRoute();
+const props = defineProps({
+  by: String,
+  image: String,
+  text: String,
+  to: String,
+});
+const { by, image, text, to } = toRefs(props);
+
+let isHover = ref(false);
+</script>
+
+<template>
+  <div>
+    <RouterLink :to="to" class="relative">
+      <div
+        class="flex items-center bg-[#1781f0] rounded-sm h-[54px] relative"
+      ></div>
+    </RouterLink>
+  </div>
+</template>
