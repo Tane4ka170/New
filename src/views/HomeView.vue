3e3f65593ecc4130f99b7f9f7631e7e691eb8b27
Tane4ka170 <tane4ka170@gmail.com>
2024-11-28 17:25:12 +0200

    Added BlendsInfluencedBy examples (HomeView.vue)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,6 +23,43 @@ import BlendsInfluencedBy from "../components/BlendsInfluencedBy.vue";
             to="/artist"
             image="https://is1-ssl.mzstatic.com/image/thumb/Music2/v4/0c/c8/0f/0cc80f43-d7d1-31b5-724a-8b41b22787de/886443998551.jpg/1200x1200bb.jpg"
           />
+          <BlendsInfluencedBy
+            class="pb-1.5"
+            text="Kedvesem"
+            by="ByeAlex"
+            to="/artist"
+            image="https://cdn-images.dzcdn.net/images/cover/93c395ff3734e20bbc2f9cb064bc2e2d/0x1900-000000-80-0-0.jpg"
+          />
+          <BlendsInfluencedBy
+            class="pb-1.5"
+            text="Human"
+            by="OneRepublic"
+            to="/artist"
+            image="https://store.onerepublic.com/cdn/shop/files/1R_OhMyMy_AlbumCover.jpg?v=1686938810"
+          />
+        </div>
+        <div class="xl:w-1/3 w-1/2">
+          <BlendsInfluencedBy
+            class="pb-1.5"
+            text="Only Teardrops"
+            by="Emilie de Forest"
+            to="/artist"
+            image="https://is1-ssl.mzstatic.com/image/thumb/Music2/v4/0c/c8/0f/0cc80f43-d7d1-31b5-724a-8b41b22787de/886443998551.jpg/1200x1200bb.jpg"
+          />
+          <BlendsInfluencedBy
+            class="pb-1.5"
+            text="Kedvesem"
+            by="ByeAlex"
+            to="/artist"
+            image="https://cdn-images.dzcdn.net/images/cover/93c395ff3734e20bbc2f9cb064bc2e2d/0x1900-000000-80-0-0.jpg"
+          />
+          <BlendsInfluencedBy
+            class="pb-1.5"
+            text="Human"
+            by="OneRepublic"
+            to="/artist"
+            image="https://store.onerepublic.com/cdn/shop/files/1R_OhMyMy_AlbumCover.jpg?v=1686938810"
+          />
         </div>
       </div>
     </div>
