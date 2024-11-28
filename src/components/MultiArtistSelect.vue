1b9b6b7655a1165ee0f568a5419f19570b362bfd
Tane4ka170 <tane4ka170@gmail.com>
2024-11-28 19:31:03 +0200

    Created MultiArtistSelect component

---
+++ src/components/MultiArtistSelect.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { toRefs, ref } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+
+import Play from "vue-material-design-icons/Play.vue";
+
+const route = useRoute();
+const props = defineProps({
+  category: String,
+  images: Object,
+  text: String,
+  to: String,
+});
+const { by, image, text, to } = toRefs(props);
+
+let isHover = ref(false);
+</script>
+
+<template>
+  <div>
+    <RouterLink :to="to" class="relative">
+      <div
+        @mouseenter="() => isHover(true)"
+        @mouseleave="() => isHover(false)"
+        class="flex justify-center items-center contrast-[1] brightness-[0.87] rounded-md aspect-square overflow-hidden"
+      >
+        <div
+          :class="
+            isHover
+              ? 'transition ease-in duration-150 bg-opacity-20'
+              : 'transition ease-out duration-150 bg-opacity-5'
+          "
+          class="absolute w-full h-full bg-black z-10 rounded-md"
+        >
+          <img
+            :src="images.one"
+            alt=""
+            class="absolute contrast-[0.55] right-0 top-0 w-1/2"
+          />
+          <img
+            :src="images.two"
+            alt=""
+            class="absolute contrast-[0.55] right-0 top-0 w-1/2"
+          />
+          <img
+            :src="images.three"
+            alt=""
+            class="absolute contrast-[0.55] right-0 top-0 w-1/2"
+          />
+          <img
+            :src="images.four"
+            alt=""
+            class="absolute contrast-[0.55] right-0 top-0 w-1/2"
+          />
+        </div>
+      </div>
+    </RouterLink>
+  </div>
+</template>
